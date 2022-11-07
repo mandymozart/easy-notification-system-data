4ce3644dcdfcfa428426a82969d3e00ebdf16e9a
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-08 00:06:07 +0100

    add domain

--- src/schemas/webUser.js
+++ src/schemas/webUser.js
@@ -12,6 +12,13 @@ export default {
       name: "fullName",
       title: "Full Name",
       type: "string",
-    }
+    },
+    {
+      name: "domain",
+      title: "Domain",
+      type: "string",
+      description:
+        "Be aware that changing the domain will make all previews notes unaccesable for that user",
+    },
   ],
 };
