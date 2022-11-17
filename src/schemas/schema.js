ae1dd9cb439cd6c423fcbd64856d2c35356ea772
Mandy Mozart <mandymozart@viennastruggle.com>
2022-11-17 17:11:58 +0100

    remove domain data model

--- src/schemas/schema.js
+++ src/schemas/schema.js
@@ -15,6 +15,6 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    note, webUser, domain
+    note, webUser
   ]),
 })
